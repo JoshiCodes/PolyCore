257696d701343edce6bb47ba8a0c2818ff00a132
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-02-12 09:44:29 +0100

    Revert "test"
    This reverts commit 4c355430235495b79051a9333ecb24e94b8ee6ae.

--- src/main/java/de/joshicodes/polycore/PolyCore.java
+++ src/main/java/de/joshicodes/polycore/PolyCore.java
@@ -20,6 +20,8 @@ import org.glassfish.tyrus.server.Server;
 import java.io.*;
 import java.util.Objects;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PolyCore {
 
     public static void main(String[] args) throws IOException {
